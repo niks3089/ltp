a5b11bad79be426babe186d8b9df8aff0d3e2d0a
Nikhil AP <niks3089@gmail.com>
2018-11-07 10:28:34 -0600

    Add an optional parameter

--- testcases/kernel/syscalls/sendto/sendto_infinite_socket.sh
+++ testcases/kernel/syscalls/sendto/sendto_infinite_socket.sh
@@ -1,15 +1,21 @@
-if [ $# -ne 3 ]; then
-    echo "sendto_infinite_tap.sh <Number of programs> <cpu_start> <tcp/udp>"
+if [ $# -lt 3 ]; then
+    echo "sendto_infinite_tap.sh <Number of programs> <cpu_start> <tcp/udp>  [optional: tap offset]"
     exit 1
 fi
 
 curr_interface=0
 cpu=$2
 trans=$3
-while [ $curr_interface -lt $1 ]
+
+if [ $# -eq 4 ]; then
+curr_interface=$4
+fi
+end=`expr $1 + $curr_interface`
+
+while [ $curr_interface -lt $end ]
 do
     curr_interface=`expr $curr_interface + 1`
     echo Starting program to send packets via $trans as server ip: $((10 + $curr_interface)).0.0.100 on cpu $cpu
-    timeout 60 ./sendto$trans -c $cpu -l -1 -m $((10 + $curr_interface)).0.0.100 &
+    ./sendto$trans -c $cpu -l -1 -m $((10 + $curr_interface)).0.0.100 &
     cpu=`expr $cpu + 1`
 done
--- testcases/kernel/syscalls/sendto/sendto_infinite_tap.sh
+++ testcases/kernel/syscalls/sendto/sendto_infinite_tap.sh
@@ -1,11 +1,17 @@
-if [ $# -ne 2 ]; then
-    echo "sendto_infinite_tap.sh <Number of programs> <cpu_start>"
+if [ $# -lt 2 ]; then
+    echo "sendto_infinite_tap.sh <Number of programs> <cpu_start> [optional: tap offset]"
     exit 1
 fi
 
 curr_interface=0
+
+if [ $# -eq 3 ]; then
+curr_interface=$3
+fi
+
 cpu=$2
-while [ $curr_interface -lt $1 ]
+end=`expr $1 + $curr_interface`
+while [ $curr_interface -lt $end ]
 do
     curr_interface=`expr $curr_interface + 1`
     echo Starting program to send packets on tap10$curr_interface on cpu $cpu
